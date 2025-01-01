58725ac77dada506267e141bbb0463f8f82d8ab1
Yamakks <maxim5_91@mail.ru>
2025-01-01 22:59:26 +0300

    update

--- src/main/java/ru/job4j/cache/DirFileCache.java
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -1,17 +1,33 @@
 package ru.job4j.cache;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class DirFileCache extends AbstractCache<String, String> {
 
     private final String cachingDir;
 
     public DirFileCache(String cachingDir) {
+        if (cachingDir == null || cachingDir.isBlank()) {
+            throw new IllegalArgumentException("Дирректория не может быть null или empty");
+        }
         this.cachingDir = cachingDir;
     }
 
 
     @Override
     protected String load(String key) {
+        Path filePath = Paths.get(cachingDir, key);
+        if (!Files.exists(filePath)) {
+            throw new IllegalArgumentException("Файл не найден:" + filePath);
+        }
 
-        return null;
+        try {
+            return Files.readString(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка чтения файла:" + filePath, e);
+        }
     }
 }
